54649f103aa9ccd5fef747d54c57ae80fe38d9d1
Ian Glover <ian.glover@gmail.com>
2016-01-16 22:01:34 +0000

    Add ticking.

--- main.elm
+++ main.elm
@@ -3,28 +3,62 @@ module Main where
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, targetValue)
-import StartApp.Simple as StartApp
 import Clock
+import String exposing (toFloat)
+import Time
+import Task exposing (Task)
 
-update : String -> String -> String
-update newValue oldValue =
-    newValue
+type alias Model =
+    { speed : Float
+    , ship_time : Float
+    , earth_time : Float
+    }
 
-view : Signal.Address String -> String -> Html
-view address string =
+init : Float -> Model
+init s =
+    { speed = s
+    , ship_time = 0
+    , earth_time = 0
+    }
+
+-- UPDATE
+
+type Action
+    = Reset
+    | Tick
+    | SetSpeed String
+
+actions = Signal.mailbox Tick
+
+update : Action -> Model -> Model
+update action model =
+    case action of
+        Reset ->
+            { model | ship_time = 0, earth_time = 0 }
+        Tick ->
+            { model | ship_time = model.ship_time + model.speed
+                    , earth_time = model.earth_time + 1 }
+        SetSpeed sliderValue ->
+            case String.toFloat sliderValue of
+                Ok value -> { model | speed = value }
+                Err msg -> { model | speed = 0 }
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
     div []
-        [ div [] [ text string ]
+        [ div [] [ text (toString model.speed) ]
         , input [ type' "range"
-                , value string
+                , value (toString model.speed)
                 , Html.Attributes.min "0"
                 , Html.Attributes.max "100"
-                , on "change" targetValue (Signal.message address)
+                , on "change" targetValue (\x -> Signal.message address (SetSpeed x))
                 ] []
-        , Clock.view "" ""                                                                 
+        , Clock.view model.ship_time
         ]
-        
+
+port tick : Signal (Task x ())
+port tick = Signal.map (\t -> Signal.send actions.address Tick) (Time.every Time.second)
 
 main =
-  StartApp.start { model = "50", 
-                   view = view,
-                   update = update }
+    Signal.map (view actions.address) <| Signal.foldp update (init 50.0) actions.signal
